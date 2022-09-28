7e8b209cbc0a677166f1943dec57c7135d1d1c2a
shahadot99999 <shahadot99999@gmail.com>
2022-09-29 00:21:52 +0600

    header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../../images/Logo.svg';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+                <h2>KLA Famous Zim</h2>
+            </div>
+
+        </nav>
+    );
+};
+
+export default Header; <h2>This is Header</h2>
