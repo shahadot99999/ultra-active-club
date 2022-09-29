c3be889efd8f70017be29d86290fe1c8bb806b1b
shahadot99999 <shahadot99999@gmail.com>
2022-09-29 12:36:43 +0600

    not add breakpoint and fail

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,20 @@
 import React from 'react';
+
 import './Cart.css';
+
+// const [break, setBreak] = useState();
+const handleToAddBreak = () => {
+    console.log('clicked');
+    // const newBreak = break;
+    // setBreak(newBreak)
+}
+
+
 
 const Cart = ({ cart }) => {
     // const { cart } = props;
+
+
 
     let time = 0;
     for (const product of cart) {
@@ -17,11 +29,11 @@ const Cart = ({ cart }) => {
 
             <div>
                 <h3>Add a Break Time</h3>
-                <div>
-                    <button>10s</button>
-                    <button>20s</button>
-                    <button>30s</button>
-                    <button>40s</button>
+                <div onClick={handleToAddBreak}>
+                    <button >10s</button>
+                    <button> 20s</button>
+                    <button> 30s</button>
+                    <button> 40s</button>
                 </div>
             </div>
 
@@ -29,7 +41,7 @@ const Cart = ({ cart }) => {
                 <h2>Exercise Details</h2>
                 <p>setCart Item: {cart.length}</p>
                 <p>Exercise time:{time}s </p>
-                <p>Break time:</p>
+                <p>Break time:{handleToAddBreak}</p>
             </div>
         </div>
     );
